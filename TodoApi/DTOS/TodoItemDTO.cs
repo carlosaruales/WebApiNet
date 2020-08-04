0c1a09bd7a167fdd1d28172da39dc4e91353b8f7
Andres ruales <carlosa.ruales@gmail.com>
2020-08-04 11:59:43 -0500

    API RestFull Core.Net

---
+++ TodoApi/DTOS/TodoItemDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TodoApi.DTOS
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
